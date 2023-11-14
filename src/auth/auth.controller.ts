09a24815cdf622c1b5c1e2c8854bdbb1acc2a7f5
Chingis Ochirov <chingis09@gmail.com>
2023-11-14 21:51:48 +0800

    feature: login dto added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,16 +8,19 @@ import {
   UseGuards,
   Request,
 } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { SignInDto } from './dto/SignInDto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Login' })
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
+  signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
